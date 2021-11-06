6e3ec4fe2085adc2baa8a5b6c2f4dfb205f9493f
esracoskun <esranur.coskunn@gmail.com>
2021-11-06 23:34:32 +0300

    ilk java kodum

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int integer;
+        float floatt;
+        double doublee;
+        long loong;
+        boolean bool;
+        String characters;
+
+        integer = 73;
+        floatt = 3.14F;
+        doublee = 3.89;
+        loong = 1231231221231231232L;
+        bool = true;
+        characters = "Selam Buuse";
+
+//        System.out.println("FLOAT: " + floatt);
+//        System.out.println("INT: " + integer);
+//        System.out.println("DOUBLE : " + doublee);
+//        System.out.println("LONG : " + loong);
+//        System.out.println("BOOLEAN : " + bool);
+//        System.out.println("STRING : " + characters);
+
+//        if (73 < 21) {
+//            System.out.println("hata");
+//        } else {
+//            System.out.println("doğru");
+//        }
+
+//        for(int i = 0; i<=10; i++) {
+//            System.out.println(i);
+//            if (i == 10) {
+//                System.out.println("Son");
+//            } else if (i == 9){
+//                System.out.println("sona yakalaşmya 1 adım kaldı");
+//            }
+//        }
+//        System.out.println("döngü bitti h.o");
+
+//        int i = 1;
+//        while(i < 10) {
+//            System.out.println(i);
+//            if (i == 7) {
+//                System.out.println("Son");
+//                do
+//            } else if (i == 9){
+//                System.out.println("sona yakalaşmya 1 adım kaldı");
+//            }
+//            i++;
+//        }
+    }
+}
+
+
+
